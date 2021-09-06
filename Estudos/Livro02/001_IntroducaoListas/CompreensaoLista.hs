1d0152f6388b3fdab531bf88ae6243563c76335f
Guilherme Rosário Alves <guilhermerosariosalgado@gmail.com>
2021-09-06 10:49:44 -0300

    Update CompreensaoLista.hs

--- Estudos/Livro02/001_IntroducaoListas/CompreensaoLista.hs
+++ Estudos/Livro02/001_IntroducaoListas/CompreensaoLista.hs
@@ -7,3 +7,17 @@ boomBangs xs = [if x > 10 then "BANG!" else "BOOM!" | x <- xs, odd x]
 --Somar elementos de duas listas
 sumListValues :: [Int] -> [Int] -> [Int]
 sumListValues xs ys = [x + y | x <- xs, y <- ys]
+
+--Minha propria versao do length
+length' xs = sum [1 | _ <- xs]
+
+--Recebe uma string e remove tudo, execto letras maisculas da lista
+removeNonUpperCasse :: String -> String
+removeNonUpperCasse xs = [x | x <- xs, x `elem` ['A'..'Z']]
+
+--gerar todos os trinangulos com lados maiores ou iguais a 10
+triangles = [(a, b, c) | a <- [1..10], b <- [1..10], c <- [1..10]]
+--e que seja um triangulo retangulo 
+rightTriangles = [(a, b, c) | a <- [1..10], b <- [1..10], c <- [1..10], a ^ 2 + b ^ 2 == c ^ 2]
+--e que o seu perimetro seja 24
+rightTriangles' = [(a, b, c) | a <- [1..10], b <- [1..a], c <- [1..10], a ^ 2 + b ^ 2 == c ^ 2, a + b+ c == 24]
