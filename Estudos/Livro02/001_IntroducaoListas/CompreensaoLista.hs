b8bfd61c20ff6bbcdbeae687e25adf6f1befefa7
Guilherme Rosário Alves <guilhermerosariosalgado@gmail.com>
2021-09-06 09:00:38 -0300

    Update CompreensaoLista.hs

--- Estudos/Livro02/001_IntroducaoListas/CompreensaoLista.hs
+++ Estudos/Livro02/001_IntroducaoListas/CompreensaoLista.hs
@@ -1,2 +1,9 @@
 module CompreensaoLista where
 
+--Trocar numeros impares maiores que 10 por "BANG!", se forem impares menores "BOOM!", se for par nem aparece
+boomBangs :: [Int] -> [String]
+boomBangs xs = [if x > 10 then "BANG!" else "BOOM!" | x <- xs, odd x]
+
+--Somar elementos de duas listas
+sumListValues :: [Int] -> [Int] -> [Int]
+sumListValues xs ys = [x + y | x <- xs, y <- ys]
