f98ed435630370b2cbe176fbbb37b2731904b562
Guilherme Rosário Alves <guilhermerosariosalgado@gmail.com>
2021-09-06 16:31:00 -0300

    Update Exe009.hs

--- Estudos/UFS/Aula06/Exe009.hs
+++ Estudos/UFS/Aula06/Exe009.hs
@@ -1,4 +1,21 @@
 module Exe009 where
+import Data.Char
 
-ehMinuscolo :: Char -> Bool
-ehMinuscolo c = c `elem` ['a'..'z']
+--versao professora
+ehMinusculo :: Char -> Bool
+ehMinusculo c = ('a' <= c) && (c <= 'z')
+
+--minha versao
+ehMinusculo' :: Char -> Bool
+ehMinusculo' c = c `elem` ['a'..'z']
+
+--versao professora
+paraMaiusculo :: Char -> Char
+paraMaiusculo c
+    | ehMinusculo c = chr (ord c - ord 'a' + ord 'A')
+    | otherwise = c
+
+--minha versao
+paraMaiusculo' :: Char -> Char
+paraMaiusculo' c | c `elem` ['a'..'z'] = (toUpper c)
+                 | otherwise = c       
