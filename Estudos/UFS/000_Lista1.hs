86e50b97a6d2f1b9877fed78c8d48a06553a5a56
Guilherme Rosário Alves <guilhermerosariosalgado@gmail.com>
2021-09-12 14:29:48 -0300

    Create 000_Lista1.hs

---
+++ Estudos/UFS/000_Lista1.hs
@@ -0,0 +1,20 @@
+module Lista1 where
+
+--Tipo mais externo, para cadastro
+type Cadastro = [Cidadao]
+
+--Tipos para cada cadastro
+type CPF = Integer
+type Nome = String
+type Genero = Char
+type Dia = Int
+type Mes = Int
+type Ano = Int
+type Data = (Dia, Mes, Ano)
+type DataNasc = Data
+type Endereco = String
+type Municipio = String
+type Estado = String
+type Telefone = String
+type Email = String 
+type Cidadao = (CPF, Nome, Genero, DataNasc, Endereco, Municipio, Estado, Telefone, Email)
