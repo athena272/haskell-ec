709991e65d631a4d2a0a745af4dcee700f504736
Guilherme Rosário Alves <guilhermerosariosalgado@gmail.com>
2021-09-18 21:14:41 -0300

    Update Exe041.hs

--- Estudos/UFS/004_Semana/Exe041.hs
+++ Estudos/UFS/004_Semana/Exe041.hs
@@ -2,5 +2,5 @@ module Exe041 where
 
 --Dadas duas listas de palavras, sendo uma de adjetivos e outra  de nomes, construa uma lista que explora todas as combinações de adjetivos e nomes das duas listas. Ex: adjetivos = [“bonito”, “alegre”], nomes=[“menino”, “garoto”]. A resposta deve ser [“menino bonito”, “menino alegre”, “garoto bonito”, “garoto alegre”]
 
-adjetivos :: [String] -> [String] -> [String]
-adjetivos lista1 lista2 = [elem2 ++ " " ++ elem1 | elem1 <- lista1, elem2 <- lista2]
+juntar :: [String] -> [String] -> [String]
+juntar adjetivos nomes = [nome ++ " " ++ adjetivo | adjetivo <- adjetivos, nome <- nomes]
