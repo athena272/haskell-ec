d0629abe2f49b33a564a9e1e32be9c6bacdc88f9
Guilherme Rosário Alves <guilhermerosariosalgado@gmail.com>
2021-09-19 02:21:05 -0300

    add Sistema Biblioteca Haskell

--- Estudos/UFS/004_Semana/SistemaBiblioteca.hs
+++ Estudos/UFS/004_Semana/SistemaBiblioteca.hs
@@ -1,4 +1,4 @@
-module FinalSemana004 where
+module SistemaBiblioteca where
 
 --Considere um banco de dados de biblioteca com os seguintes tipos
 type Pessoa = String
@@ -6,7 +6,7 @@ type Livro = String
 type Emprestimos = [(Pessoa, Livro)]
 
 meuBancoDados :: Emprestimos
-meuBancoDados = [("ivo", "livro A"), ("carlos", "livro B"),("joana", "livro C"),("gui", "livro D"),("pedrino", "livro A")] 
+meuBancoDados = [("ivo", "livro A"), ("carlos", "livro B"),("joana", "livro C"),("gui", "livro D"),("pedrino", "livro A"), ("gui", "livro E"), ("gui", "livro F"), ("ivo", "livro G")] 
 --Escreva as seguintes funções:
 
 --Dada uma pessoa, encontre os livros que ela emprestou;
@@ -19,10 +19,17 @@ finPessoaEmprestou livro = [pessoaComLivro | (pessoaComLivro,livroEmprestado) <-
 
 --Dado um livro, desejamos saber se o mesmo se encontra emprestado ou não;
 livroEstaEmprestado :: Livro -> Bool
-livroEstaEmprestado livro = head [livroEmprestado == livro| (_,livroEmprestado) <- meuBancoDados]
+livroEstaEmprestado livro = or [livroEmprestado == livro| (_,livroEmprestado) <- meuBancoDados]
 
 --Dada uma pessoa, desejamos saber a quantidade de livros que ela tomou emprestado;
+quantosLivros:: Pessoa -> Int
+quantosLivros nome = length [livro | (pessoaComLivro,livro) <- meuBancoDados, pessoaComLivro == nome]
 
 --Dado um par (Pessoa, Livro), queremos adicioná-lo  à lista de emprestados, sinalizando o empréstimo realizado;
+addEmprestimo :: (Pessoa, Livro) -> Emprestimos
+addEmprestimo (pessoa, livro) = (:) (pessoa, livro) meuBancoDados --maneira infixa
 
 --Dado um par (Pessoa, Livro), queremos removê-lo  da lista de emprestados, sinalizando a sua devolução.
+addEmprestimo :: (Pessoa, Livro) -> Emprestimos
+addEmprestimo (pessoa, livro) 
+    | (pessoa, livro) `elem` meuBancoDados -- se estiver no banco, retirar o elemento 
