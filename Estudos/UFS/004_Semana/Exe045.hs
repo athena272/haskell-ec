bcf5c62865a4b217c797728de7e7dba519fcaddf
Guilherme Rosário Alves <guilhermerosariosalgado@gmail.com>
2021-09-19 00:39:34 -0300

    Update Exe045.hs

--- Estudos/UFS/004_Semana/Exe045.hs
+++ Estudos/UFS/004_Semana/Exe045.hs
@@ -5,7 +5,9 @@ listaReais :: [Float] -> Float
 listaReais lista = sum [elementos | elementos <- lista, elementos >= 5]
 
 --Dada uma lista de reais, elabore uma função para calcular a média dos valores da lista
-listaNum :: [Float] -> Float
-listaNum lista = (sum lista) / fromIntegral (length lista)
+listaMedia :: [Float] -> Float
+listaMedia lista = (sum lista) / fromIntegral (length lista)
 
 --Dada uma lista de notas de alunos, elabore uma função para determinar a lista das notas acima da média das notas da lista.
+listaNotasAcimaMedia :: [Float] -> [Float]
+listaNotasAcimaMedia listaNotas = [notas | notas <- listaNotas, notas >= (listaMedia listaNotas)]
