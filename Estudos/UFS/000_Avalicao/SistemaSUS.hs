647429dde75c362e7e28450e13f8bfdd02649bb8
Guilherme Rosário Alves <guilhermerosariosalgado@gmail.com>
2021-09-20 08:12:11 -0300

    Update SistemaSUS.hs

--- Estudos/UFS/000_Avalicao/SistemaSUS.hs
+++ Estudos/UFS/000_Avalicao/SistemaSUS.hs
@@ -33,32 +33,22 @@ addCadastroSUS myCidadao myDataBase
     
 --item b)O cidadão pode querer modificar algum desses dados, por exemplo, o número de telefone ou endereço. Para isto, precisamos de funções de atualização dos dados no cadastro, passando os novos dados. Para simplificar o sistema, vamos supor apenas as funções de atualização do endereço e do telefone, já que as demais atualizações seguiriam o mesmo princípio. No processo de atualização, o cadastro SUS informado será copiado para um novo cadastro SUS. Neste novo cadastro, os registros de outros cidadãos permanecerão inalterados e somente os dados do cidadão que está sendo atualizado sofrerão modificações.
 atualizaEnderecoSUS :: CPF -> CadastroSUS -> Endereco -> CadastroSUS
-atualizaEndSUS num_cpf banco novo_endereco = parteUm ++ [(cpf,a,b,c,novo_endereco,e,f,g,h)| (cpf,a,b,c,endereco,e,f,g,h) <- banco, cpf == num_cpf] ++ parteDois
-    where pos = encontraPosElem num_cpf banco
-          parteUm = take (pos-1) banco
-          parteDois = drop pos banco
+atualizaEnderecoSUS myCPF myDataBase newAdress = comeco ++ [(cpf,nome,gender,nasc,newAdress,muni,state,tel,email)| (cpf,nome,gender,nasc,newAdress,muni,state,tel,email) <- myDataBase, cpf == myCPF] ++ fim
+    where position = findPosElem myCPF myDataBase
+          comeco = take (position - 1) myDataBase
+          fim = drop position myDataBase
 
+atualizaTelefoneSUS :: CPF -> CadastroSUS -> Telefone -> CadastroSUS
+atualizaTelefoneSUS myCPF myDataBase newTel = comeco ++ [(cpf,nome,gender,nasc,adress,muni,state,newTel,email) |  (cpf,nome,gender,nasc,adress,muni,state,newTel,email) <- myDataBase, cpf == myCPF] ++ fim
+    where position = findPosElem myCPF myDataBase
+          comeco = take (position - 1) myDataBase
+          fim = drop position myDataBase
 
+--item c) Quando um cidadão falece, a família tem que notificar o fato em um posto de saúde, para que ele seja retirado do cadastro corrente do SUS. Como há uma verificação do atestado de óbito, isto só pode ser feito no posto. O sistema precisará da função abaixo. Se o CPF existir no cadastro corrente do SUS, o registro do cidadão deve ser completamente excluído, gerando um novo cadastro sem os dados deste cidadão. Se o CPF não existir, uma mensagem de erro, usando error, sinalizando que o cidadão não pertence ao cadastro deve ser exibida.
+cidadaoMortoSUS :: CPF -> CadastroSUS -> CadastroSUS
+cidadaoMortoSUS myCPF myDataBase = [(cpf,nome,gender,nasc,adress,muni,state,tel,email) | (cpf,nome,gender,nasc,adress,muni,state,tel,email) <- myDataBase, myCPF /= cpf]
 
---PROCURANDO ELEMENTO PARA ATUALIZAÇÃO DE DADOS
-
-posElementosLista :: CadastroSUS -> [(Int, Cidadao)]
-indexarLista myDataBase = zip posicoes myDataBase
-    where posicoes = [1..(length myDataBase)]
-
-encontraPosElem :: CPF -> CadastroSUS -> Int
-encontraPosElem num_cpf banco 
-    |null ps = 0
-    |otherwise = head ps
-    where ps = [ indice| (indice, pessoa) <- indexarLista banco, pegarCPF pessoa ==num_cpf]
-
-
---PEGANDO SOMENTE O ENDEREÇO
-pegarEndereco :: Cidadao -> Endereco
-pegarEndereco (,,,,endereco,,,,) = endereco
-
-
-
+--item d)
 
 
 --GETS e outras funçoes auxiliares
@@ -74,3 +64,15 @@ getTelefone (_, _, _, _, _, _, _, myTelefone, _) = myTelefone
 ----Para cadastrar um novo cidadão, inicialmente é checado se o CPF já existe ou não no sistema com a função 
 checkCPF :: CPF -> CadastroSUS -> Bool
 checkCPF myCPF myDataBase = or [myCPF == cpfDataBase| (cpfDataBase, _, _, _, _, _, _, _, _) <- myDataBase] --Se pelo menos um for verdadeiro na lista, já é o bastante, por isso a funcao "or"
+
+--Atribuir um valor(indixe) para cada cidadao que exisitir no "banco de dados"
+posicionarElementosLista :: CadastroSUS -> [(Int, Cidadao)]
+posicionarElementosLista myDataBase = zip posicoes myDataBase
+    where posicoes = [1..(length myDataBase)]
+
+--Encontrar a posicao do cidadao com base no seu CPF
+findPosElem :: CPF -> CadastroSUS -> Int
+findPosElem myCPF myDataBase
+    |null posicao = 0
+    |otherwise = head posicao
+    where posicao = [ position | (position, cidadao) <- (posicionarElementosLista myDataBase), (getCPF cidadao)  == myCPF]
