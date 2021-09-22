a260a04a07bf5aec890dfc29bbd23181afe8f964
Guilherme Rosário Alves <guilhermerosariosalgado@gmail.com>
2021-09-22 10:37:57 -0300

    Update SistemaSUS.hs

--- Estudos/UFS/000_Avalicao/SistemaSUS.hs
+++ Estudos/UFS/000_Avalicao/SistemaSUS.hs
@@ -21,7 +21,7 @@ type Cidadao = (CPF, Nome, Genero, DataNasc, Endereco, Municipio, Estado, Telefo
 
 --Meus cadastros pre-existentes no banco de dados 
 bancoDeCadastros :: CadastroSUS
-bancoDeCadastros = [(26716347665, "Paulo Souza", 'M', (11,10,1996),"Rua A, 202","Muribeca", "SE", "999997000", "psouza@gmail.com"),(87717347115, "Ana Reis",'F', (5,4,1970), "Rua B, 304","Aracaju", "SE", "999826004", "areis@gmail.com"), (99999999999, "Paulo Souza", 'M', (11,10,1996),"Rua A, 202","Muribeca", "SE", "999997000", "psouza@gmail.com")]
+bancoDeCadastros = [(26716347665, "Paulo Souza", 'M', (11,10,1996),"Rua A, 202","Muribeca", "SE", "999997000", "psouza@gmail.com"),(87717347115, "Ana Reis",'F', (5,4,1970), "Rua B, 304","Aracaju", "SE", "999826004", "areis@gmail.com"),(99999999999, "Guilherme Alves", 'M', (02,07,2002),"Rua C, 405","Salgado", "SE", "999997044", "guilherme@gmail.com"), (88888888888, "Esmeralda Oliveira", 'F', (09,09,2003),"Rua D, 506","Lagarto", "SE", "999996025", "esmeralda@gmail.com")]
 
 -- item a) Cadastramento de um cidadão no sistema. 
 --Para cadastrar um novo cidadão, inicialmente é checado se o CPF já existe ou não no sistema com a função 
