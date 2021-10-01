f8022e86221091099af9aab265258133141e1bf8
Guilherme Rosário Alves <guilhermerosariosalgado@gmail.com>
2021-09-30 21:57:45 -0300

    Update Matheus.hs

--- Estudos/UFS/000_Avalicao/Matheus.hs
+++ Estudos/UFS/000_Avalicao/Matheus.hs
@@ -4,38 +4,37 @@ module ProvaMatheus where
 somatoria :: Float -> Float -> Float 
 somatoria limInf limSup = sum [sin (2 * pi / a) | a <- [limInf..limSup]]
 
+--Questao de somatoria
+somatoria' :: Float -> Float -> Float 
+somatoria' limInf limSup = sum [(1 / (sqrt a)) | a <- [limInf..limSup]]
+
 --Questao tres chars
 dadoTresChars :: Char -> Char -> Char -> Int
 dadoTresChars c1 c2 c3 = (ehMaisculo c1) + (ehMaisculo c2) + (ehMaisculo c3)
+
+semOrdem :: [Int] -> Bool
+semOrdem listaNum 
+    | temZero listaNum = error "lista invalida"
+    | verificaNegativo (snd (quebraListaEmDuasParte1 listaNum)) = True 
+    | otherwise = False
+
+quebraListaEmDuasParte1 :: [Int] -> ([Int], [Int])
+quebraListaEmDuasParte1 list = span (< 0) list
+quebraListaEmDuasParte2 :: [Int] -> ([Int], [Int])
+quebraListaEmDuasParte2 list = span (> 0) list
+
+
+verificaNegativo :: [Int] -> Bool
+verificaNegativo list = or [num < 0| num <- list]
+temZero :: [Int] -> Bool
+temZero list = or [num == 0| num <- list]
+
+
+--Questao tres chars
+--dadosQuatroChars :: Char -> Char -> Char -> Int
+--dadoQuatroChars c1 c2 c3 = (ehMaisculo c1) + (ehMaisculo c2) + (ehMaisculo c3)
 
 ehMaisculo :: Char -> Int 
 ehMaisculo letra 
     | letra `elem` ['A'..'Z'] = 1
     | otherwise = 0 
-
---Questao sem ordem
-semOrdem :: [Int] -> Bool
-semOrdem listaNum 
-    --tratamento para erro quando tiver 0 na lista
-    | temZero listaNum = error "lista invalida"
-    --Se o segundo elemento da tupla, que eh uma lista, tive algum negativo, eles estao misturados
-    | verificaNegatico (snd (quebraListaEmDuas listaNum)) = True 
-    --Caso contrario eh falso
-    | otherwise = False  
-{--
-transforma a lista em uma tupla
-O primeiro elemento da tupla é uma lista com os elems da lista dado ate que um elems seja positivo
-O segundo elemento da tupla é uma lista com os restos do elementos
---}
-quebraListaEmDuas :: [Int] -> ([Int], [Int])
-quebraListaEmDuas list = span (< 0) list
-{--
-verificar se tem numeros negaticos(usada para ver se na segunda parte da tupla, existe uma lista com negativos)
---}
-verificaNegatico :: [Int] -> Bool
-verificaNegatico list = or [num < 0| num <- list]
-{--
-verifica se tem zero na lista 
---}
-temZero :: [Int] -> Bool
-temZero list = or [num == 0| num <- list]
