86aac09e1bf66b6dd9775e7b96e99012412fae9e
Guilherme Rosário Alves <guilhermerosariosalgado@gmail.com>
2021-10-08 15:35:39 -0300

    Update 006_BolinhaAndando.hs

--- Estudos/UFS/003_Semana/006_BolinhaAndando.hs
+++ Estudos/UFS/003_Semana/006_BolinhaAndando.hs
@@ -1,10 +1,16 @@
 {-# LANGUAGE OverloadedStrings #-}
 import CodeWorld
  
-main = animationOf bolaHorizontal
+main = animationOf (bolaHV)
+
+bolaHV :: Double -> Picture
+bolaHV tempo = bolaHorizontal tempo & bolaVertical tempo
 
 bolaHorizontal :: Double -> Picture
 bolaHorizontal tempo = translated (-10 + tempo * 2) 0 bola
 
-bola = colored purple (solidCircle 1)
+bolaVertical :: Double -> Picture
+bolaVertical tempo = translated 0 (10 + (-1)/2 * grav * tempo^2) bola
+  where grav = 2
 
+bola = colored purple (solidCircle 1)
