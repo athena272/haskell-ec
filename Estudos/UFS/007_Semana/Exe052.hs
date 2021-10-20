08da0c01485567bad103f56abb9e6cf9b0ce6b2c
Guilherme Rosário Alves <guilhermerosariosalgado@gmail.com>
2021-10-20 17:44:36 -0300

    Update Exe052.hs

--- Estudos/UFS/007_Semana/Exe052.hs
+++ Estudos/UFS/007_Semana/Exe052.hs
@@ -3,3 +3,7 @@ module Exe052 where
 myConcat :: [[a]] -> [a] 
 myConcat [] = []
 myConcat (xs:xss) = xs ++ myConcat xss
+
+myReverse :: [a] -> [a]
+myReverse [] = []
+myReverse (x:xs) = myReverse xs ++ [x]
