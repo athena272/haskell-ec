e15ce862c73584f15f8ace90d9b6a8771c885022
Guilherme Rosário Alves <guilhermerosariosalgado@gmail.com>
2021-10-21 15:59:49 -0300

    Update Exe052.hs

--- Estudos/UFS/007_Semana/Exe052.hs
+++ Estudos/UFS/007_Semana/Exe052.hs
@@ -7,3 +7,11 @@ myConcat (xs:xss) = xs ++ myConcat xss
 myReverse :: [a] -> [a]
 myReverse [] = []
 myReverse (x:xs) = myReverse xs ++ [x]
+
+myrepeat :: a -> [a]
+myrepeat elem =  elem : myrepeat elem  
+
+myzip :: [a] -> [b] -> [(a, b)]
+myzip _ [] = [] 
+myzip [] _ = []
+myzip (elem1:lista1) (elem2:lista2) = (elem1, elem2) : myzip lista1 lista2
