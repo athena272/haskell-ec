81b356635bc485c421aef01face413a903f89a81
Guilherme Rosário Alves <guilhermerosariosalgado@gmail.com>
2021-10-25 04:10:04 -0300

    Update Recursao.hs

--- Estudos/Livro02/006_OlaRecursao/Recursao.hs
+++ Estudos/Livro02/006_OlaRecursao/Recursao.hs
@@ -14,3 +14,23 @@ mymaximum' :: (Ord a) => [a] -> a
 mymaximum' [] = error "Lista vazia, amador!"
 mymaximum' [x] = x
 mymaximum' (x:xs) = max x (mymaximum' xs)
+
+myReplicate :: Int -> Int -> [Int]
+myReplicate n x 
+    | n <= 0 = []
+    | otherwise = x : myReplicate (n - 1) x
+
+myTake :: Int -> [Int] -> [Int]
+myTake n _
+    | n <= 0 = []
+myTake _ [] = []
+myTake n (x:xs) = x : myTake (n - 1) xs
+
+myReverse :: [a] -> [a]
+myReverse [] = []
+myReverse (x:xs) = myReverse xs ++ [x]
+
+myZip :: [Int] -> [Int] -> [(Int, Int)] 
+myZip [] _ = []
+myZip _ [] = []
+myZip (x:xs) (y:ys) = (x, y) : myZip xs ys
