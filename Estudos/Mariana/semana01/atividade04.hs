524d571e78033a23b36d52def40199fa1fd3c450
athena272 <guilhermera272@gmail.com>
2023-06-28 23:10:38 -0300

    Delete atividade04.hs

--- Estudos/Mariana/semana01/atividade04.hs
+++
@@ -1,5 +0,0 @@
-areaTrianguloHeron :: Double -> Double -> Double -> Double
-areaTrianguloHeron ladoA ladoB ladoC = semiPerimetro * (semiPerimetro - ladoA) * (semiPerimetro - ladoB) * (semiPerimetro - ladoC) 
-  where 
-    semiPerimetro = (ladoA + ladoB + ladoC) / 2
-  
